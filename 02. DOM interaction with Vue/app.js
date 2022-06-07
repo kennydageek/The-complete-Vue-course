2dfb14748f3b72b918793848dc008d6cbfbbfa40
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-07 15:12:29 +0100

    folder restructured and exercise files added

--- 02. DOM interaction with Vue/app.js
+++ 02. DOM interaction with Vue/app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        link: 'http://google.com'
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">Google</a>'
     },
 
     methods: {
