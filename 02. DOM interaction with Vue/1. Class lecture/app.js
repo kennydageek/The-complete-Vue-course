5ae913a2240c705b8f0c4472d7332ca1c0355452
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-08 11:28:22 +0100

    listening to event with v-on and getting event data from the Event Object

--- 02. DOM interaction with Vue/1. Class lecture/app.js
+++ 02. DOM interaction with Vue/1. Class lecture/app.js
@@ -1,15 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World!',
-        link: 'http://google.com',
-        finishedLink: '<a href="http://google.com">Google</a>'
+        counter: 0,
+        x: 0,
+        y: 0,
     },
-
     methods: {
-        sayHello() {
-            this.title = 'Hello!';
-            return this.title;
+        increase() {
+            this.counter++;
         },
+
+        updateCoordinate(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+            console.log(event)
+        }
     }
 });
