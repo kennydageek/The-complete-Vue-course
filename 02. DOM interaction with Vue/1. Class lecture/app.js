6838460d274eef4a32ab6ca7c1a10272de32afc6
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-09 13:43:28 +0100

    Using two-way binding with v-model and reacting to changes with computed properties

--- 02. DOM interaction with Vue/1. Class lecture/app.js
+++ 02. DOM interaction with Vue/1. Class lecture/app.js
@@ -2,22 +2,19 @@ new Vue({
     el: '#app',
     data: {
         counter: 0,
-        x: 0,
-        y: 0,
+        secondCounter: 0,
     },
+
+    computed: {
+        output() {
+            return this.counter > 5 ? 'Greater 5' : 'Smaller than 5';
+        }
+    },
+
     methods: {
-        increase(step, event) {
-            this.counter += step;
-        },
-
-        updateCoordinate(event) {
-            this.x = event.clientX;
-            this.y = event.clientY;
-            console.log(event)
-        },
-
-        alertMe() {
-            alert('Alert!')
+        result() {
+            return this.counter > 5 ? 'Greater 5' : 'Smaller than 5';
         }
     }
+
 });
