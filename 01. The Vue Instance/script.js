73c12e95411684207a6aaf225424729055f70080
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-13 04:19:57 +0100

    Attribute binding and coding challenge done

--- 01. The Vue Instance/script.js
+++ 01. The Vue Instance/script.js
@@ -3,5 +3,7 @@ new Vue({
     data: {
         product: 'Socks',
         description: 'This is a product page for socks',
+        image: '/images/socks_green.jpg',
+        link: 'https://google.com'
     }
 })
