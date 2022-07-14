6b939c17706877d1c03d1988e968987dafa96ff0
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-07-14 13:30:58 +0100

    kdks

---
+++ components/src/components/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Server status: {{ status }}</p>
+    <hr />
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: 'critical',
+    };
+  },
+
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
