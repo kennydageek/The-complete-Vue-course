91f17cd31205126a119eb9e2a9e6751eb1f1dddc
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-07-14 12:21:49 +0100

    Got quote to display

--- Quote App/src/App.vue
+++ Quote App/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div></div>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
 export default {
   data() {
     return {
       quotes: ['Just a Quote to see something'],
       maxQuotes: 10,
     };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
   },
 };
 </script>
