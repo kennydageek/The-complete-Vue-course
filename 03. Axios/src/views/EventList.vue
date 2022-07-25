97b47a3983d141380fd8161284f4d3956729a871
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-07-25 10:51:14 +0100

    Axios starter files

---
+++ 03. Axios/src/views/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard/>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
---
+++ 03. Axios/src/views/EventShow.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Showing event #{{ id }}</h1>
+</template>
+<script>
+export default {
+  props: ['id']
+}
+</script>
