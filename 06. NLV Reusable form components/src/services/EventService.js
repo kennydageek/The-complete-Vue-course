b1239c256eb62dd107278cbc0e20f5919e39613f
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-08-25 09:57:24 +0100

    reusable form components starter files

---
+++ 06. NLV Reusable form components/src/services/EventService.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: `http://localhost:3000`,
+  withCredentials: false, // This is the default
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  timeout: 10000
+})
+
+export default {
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+  },
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  },
+  postEvent(event) {
+    return apiClient.post('/events', event)
+  }
+}
