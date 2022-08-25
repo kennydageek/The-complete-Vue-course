95866612e0f5e8ec9b3433193656841b1fa020d9
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-08-25 12:43:46 +0100

    Base button reusable component added

---
+++ 06. NLV Reusable form components/src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button v-on="$listeners" v-bind="$attrs"  class="button" :class = 'buttonClass'>
+            <slot />
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    buttonClass: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 52px;
+  padding: 0 40px;
+  background: transparent;
+  border: none;
+  border-radius: 6px;
+  text-align: center;
+  font-weight: 600;
+  white-space: nowrap;
+  transition: all 0.2s linear;
+}
+
+.button.-fill-gradient {
+  background: linear-gradient(to right, #16c0b0, #84cf6a);
+  color: #fff;
+}
+</style>
