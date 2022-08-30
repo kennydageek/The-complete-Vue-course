eccb34553c263a344246c82411d8910d24e00081
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-08-30 14:08:41 +0100

    Mixins starter files

---
+++ 08. Mixins/src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" @input="updateValue" v-bind="$attrs" v-on="listeners">
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      }
+    }
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
