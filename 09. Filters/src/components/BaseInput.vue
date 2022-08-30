5ccd6f0fc530f6958f2ed79a1ca1106f84b0fa05
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-08-30 15:13:58 +0100

    Filters starter files

---
+++ 09. Filters/src/components/BaseInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" @input="updateValue" v-bind="$attrs" v-on="listeners">
+  </div>
+</template>
+
+<script>
+import { formFieldMixin } from '../mixins/formFieldMixin'
+export default {
+  mixins: [formFieldMixin],
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      }
+    }
+  }
+}
+</script>
