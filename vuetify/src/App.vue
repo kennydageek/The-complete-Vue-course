4ed81126f2522a0c4e33f4c59d3f954f9719596c
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-09-03 09:51:55 +0100

    vuetify components pt 1

--- vuetify/src/App.vue
+++ vuetify/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-main>
+
+    <!-- Login -->
     <v-card width="400" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display-1">Login</h1>
@@ -27,6 +36,37 @@
         <v-btn color="info">Login</v-btn>
       </v-card-actions>
     </v-card>
+    </v-main>
+
+
+  <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+
+
   </v-app>
 </template>
 
@@ -37,6 +77,11 @@ export default {
   data: () => ({
     //
     showPassword: false,
+    links: [
+      'Home',
+      'Login',
+
+    ]
   }),
 };
 </script>
