3f02cbe4b1103cbc2a33a022605b23e894c1919b
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-09-04 20:33:41 +0100

    Employee dashboard starter files

---
+++ 11. Vuetify Challenge/src/views/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardPage',
+  data() {
+    return {
+      currentItem: '',
+      snackbar: false,
+    };
+  },
+  methods: {
+    selectRow(event) {
+      this.snackbar = true;
+      this.currentItem = event.name;
+    },
+  },
+};
+</script>
+
+<style></style>
