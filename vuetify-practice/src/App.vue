fcb0de0f51fcfdf7a915b4b0e004cffa86abb173
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-10-31 21:44:36 +0100

    Token based authentication starter files

--- vuetify-practice/src/App.vue
+++ vuetify-practice/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app>
+    <v-app-bar app></v-app-bar>
+    <v-container>
+      <v-row>
+        <v-col cols="12"></v-col>
+      </v-row>
+    </v-container>
     <v-main>
       <router-view />
     </v-main>
