fd4aef05c717d97f4351234597f66b5526bb4185
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-11-01 14:36:13 +0100

    authentication for register and login

--- 14. Token Based Authentication/src/router.js
+++ 14. Token Based Authentication/src/router.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Dashboard from './views/Dashboard.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Dashboard from './views/Dashboard.vue';
+import RegisterUser from './views/RegisterUser.vue';
+import LoginUser from './views/LoginUser.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
@@ -12,14 +14,25 @@ const router = new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard
-    }
-  ]
-})
+      component: Dashboard,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser,
+    },
 
-export default router
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginUser,
+    },
+  ],
+});
+
+export default router;
---
+++ 14. Token Based Authentication/src/views/RegisterUser.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form @submit.prevent="register">
+      <label for="name">
+        Name:
+      </label>
+      <input v-model="name" type="text" name="name" value />
+
+      <label for="email">
+        email:
+      </label>
+      <input v-model="email" type="text" name="email" value />
+
+      <label for="password">
+        password:
+      </label>
+      <input v-model="password" type="text" name="password" value />
+
+      <button type="submit" name="button">Register</button>
+      <router-link to="/login"> Already have an account? Login. </router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    register() {
+      this.$store
+        .dispatch('register', {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' });
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
