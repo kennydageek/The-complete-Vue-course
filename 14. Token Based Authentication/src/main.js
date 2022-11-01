7bcaecc8c465d36cd2f1340d309bfc10a10e4bd4
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-11-01 17:45:27 +0100

    Automatic Login

--- 14. Token Based Authentication/src/main.js
+++ 14. Token Based Authentication/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './vuex/store';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 
@@ -9,4 +10,20 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
+  created() {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const userData = JSON.parse(userString);
+      this.$store.commit('SET_USER_DATA', userData);
+    }
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout');
+        }
+        return Promise.reject(error);
+      }
+    );
+  },
 }).$mount('#app');
